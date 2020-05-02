165cdaeee3142665a5b4b3aaf45b8c45b8d8397f
pavankumarv1997 <pavankumarv287.pk@gmail.com>
2020-05-02 13:35:47 +0530

    App component setup

--- src/App.js
+++ src/App.js
@@ -4,11 +4,48 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Todoinput from "./components/Todoinput";
 import Todolist from "./components/Todolist";
 export default class App extends Component {
+  state = {
+    items: [
+      { id: 1, title: "Wake Up" },
+      { id: 1, title: "Work Out" },
+    ],
+    id: uuid(),
+    item: "",
+    editItem: false,
+  };
+  handleChange = (e) => {
+    console.log("Change item");
+  };
+  handleSubmit = (e) => {
+    console.log("Submit");
+  };
+  handleEdit = (id) => {
+    console.log(`Edit ${id}`);
+  };
+  handleDelete = (id) => {
+    console.log(`Delete ${id}`);
+  };
   render() {
     return (
       <>
-        <Todoinput />
-        <Todolist />
+        <div className="container">
+          <div className="row">
+            <div className="col-10 mx-auto md-8 mt-5">
+              <h3 className="text-center text-capitalize">Todo Input </h3>
+              <Todoinput
+                item={this.state.item}
+                handleChange={this.handleChange}
+                handleSubmit={this.handleSubmit}
+                editItem={this.state.editItem}
+              />
+              <Todolist
+                item={this.state.items}
+                handleEdit={this.handleEdit}
+                handleDelete={this.handleDelete}
+              />
+            </div>
+          </div>
+        </div>
       </>
     );
   }
